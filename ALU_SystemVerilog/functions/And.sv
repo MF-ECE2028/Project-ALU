c09425295368e7b5a7e94457565d3a3761572b84
John O'Connor <164067041+oconnor-john@users.noreply.github.com>
2026-06-03 20:35:40 -0700

    Add files via upload

--- ALU_SystemVerilog/functions/And.sv
+++ ALU_SystemVerilog/functions/And.sv
@@ -1,9 +1,13 @@
 B,module And (
 	input logic [7:0] A, B,
 	input logic enable,
-	output logic [7:0] Y
+	output logic [7:0] Y,
+	output logic done,
+	output logic overflow
 );
 	
 	assign Y = enable ? (A & B) : 8'bz;
+	assign done = enable ? 1'b1 : 1'bz;
+   assign overflow = enable ? 1'b0 : 1'bz;
 
 endmodule
