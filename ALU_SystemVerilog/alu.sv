d91d69a4dddfc10378fc11af0b782165955cadaa
Nolan Kessler <kesslnol@oregonstate.edu>
2026-06-05 20:42:39 -0700

    More documentation work

--- ALU_SystemVerilog/alu.sv
+++ ALU_SystemVerilog/alu.sv
@@ -19,6 +19,12 @@ module ALU_8 (
     
     logic [15:0] cs; //chip select lines
     logic reg_en = en & ~Rlk.out; //Enable or disable updating input registers
+    logic [7:0] _y;
+    logic _overflow;
+    logic _done;
+    y = _y & 8{enable}
+    overflow = _overflow & enable
+    done = _done & enable
 
     Register_en_rstn Rlk #(1) ( //Lock register - prevents output of Ra and Rb and Rop from changing after rising edge of en
         .clk(~clk),
@@ -48,14 +54,84 @@ module ALU_8 (
         .in(op)
     );
     
-    Multiplexer_4_en M_cs (
+    Multiplexer_3_en M_cs (
         .in(op),
         .enable(Rlk.out),
         .out(cs)
     );
 
-    //TODO Create instances of functions here
+    //Operation Instances
+    A_pass a_pass (
+        .A(Ra.out),
+        .enable(Rop.out[0]),
+        .Y(result),
+        .done(_done),
+        .overflow(_overflow)
+    );
 
+    B_pass a_pass (
+        .B(Rb.out),
+        .enable(Rop.out[1]),
+        .Y(result),
+        .done(_done),
+        .overflow(_overflow)
+    );
+
+    Bitwise_AND bw_and (
+        .A(Ra.out),
+        .B(Rb.out),
+        .enable(Rop.out[2]),
+        .Y(result),
+        .done(_done),
+        .overflow(_overflow)
+    );
+
+    Bitwise_OR bw_or (
+        .A(Ra.out),
+        .B(Rb.out),
+        .enable(Rop.out[3]),
+        .Y(result),
+        .done(_done),
+        .overflow(_overflow)
+    );
+
+    Bitwise_XOR bw_xor (
+        .A(Ra.out),
+        .B(Rb.out),
+        .enable(Rop.out[4]),
+        .Y(result),
+        .done(_done),
+        .overflow(_overflow)
+    );
+
+    Addition addition (
+        .A(Ra.out),
+        .B(Rb.out),
+        .enable(Rop.out[5]),
+        .Y(result),
+        .done(_done),
+        .overflow(_overflow)
+    );
+
+    Subtraction subtraction (
+        .A(Ra.out),
+        .B(Rb.out),
+        .enable(Rop.out[6]),
+        .Y(result),
+        .done(_done),
+        .overflow(_overflow)
+    );
+
+    Mult_8bit multiplication (
+        .A(Ra.out),
+        .B(Rb.out),
+        .clock(clk),
+        .reset_n(rst_n),
+        .enable(Rop.out[7]),
+        .Y(result),
+        .done(_done),
+        .overflow(_overflow)
+    )
 
 
 endmodule
---
+++ ALU_SystemVerilog/utility_modules/multiplexer_3_en.sv
@@ -0,0 +1,30 @@
+/*
+Project: 8-Bit ALU
+Author: Nolan Kessler
+
+Implements a three bit multiplexer with enable
+*/
+
+module Multiplexer_3_en (
+    input logic [2 : 0] in,
+    input logic enable, //Output of multiplexer is 0 unless enable is high.
+    output logic [7 : 0] out,
+);
+    always_comb begin
+        if (enable) begin
+            case (in)
+                3'b000: out = 16'b0000_0001;
+                3'b001: out = 16'b0000_0010;
+                3'b010: out = 16'b0000_0100;
+                3'b011: out = 16'b0000_1000;
+                3'b100: out = 16'b0001_0000;
+                3'b101: out = 16'b0010_0000;
+                3'b110: out = 16'b0100_0000;
+                3'b111: out = 16'b1000_0000;
+            endcase
+        end
+        else begin
+            out = 4'b0
+        end
+    end
+endmodule
